dca6f76e0e47e199c34288a03a397fc0028f861a
Gertobin11 <gertobin11@gmail.com>
2022-04-20 08:33:00 +0000

    added permissions and is_owner to serializer

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django_rest_framework.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -9,22 +10,26 @@ from .serializers import ProfileSerializer
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(profiles, many=True,
+                                       context={'request': request})
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
 
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     
     def get(self, request, pk):
         profile = get_object_or_404(Profile, pk=pk)
-        serializer = ProfileSerializer(profile)
+        self.check_object_permissions(self.request, profile)
+        serializer = ProfileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = get_object_or_404(Profile, pk=pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(profile, data=request.data,
+                                       context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
