1a4618c01e16c1d27a3a3d4d51807ce5d8292613
Gertobin11 <gertobin11@gmail.com>
2022-04-27 07:42:47 +0000

    refatored profiles and model views to make use of the class generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,36 +1,18 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+
+from rest_framework import generics, permissions
 from django_rest_framework.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True,
-                                       context={'request': request})
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
 
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
 
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    
-    def get(self, request, pk):
-        profile = get_object_or_404(Profile, pk=pk)
-        self.check_object_permissions(self.request, profile)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = get_object_or_404(Profile, pk=pk)
-        serializer = ProfileSerializer(profile, data=request.data,
-                                       context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
